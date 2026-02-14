7592ac0ecdff59c391635c21a9aa3e8240a6e7bc
Shabbir Habib <shabbirhabib2014@gmail.com>
2026-02-14 20:01:27 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -179,6 +179,13 @@ class Dev(Configuration):
 
     DJANGO_ADMINS="Ben Shaw,ben@example.com;Leo Lucio,leo@example.com"
 
+    PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
 class Prod(Dev):
     DEBUG = False
     SECRET_KEY = values.SecretValue()
